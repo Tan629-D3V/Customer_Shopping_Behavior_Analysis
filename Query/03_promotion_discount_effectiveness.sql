8aaf655f39949b0a1b7ef5e7f0a856f2305d4724
Tan629-DEV <tanmaychouhan826629@gmail.com>
2026-07-24 02:29:12 +0530

    update

--- Query/03_promotion_discount_effectiveness.sql
+++ Query/03_promotion_discount_effectiveness.sql
@@ -1,8 +1,12 @@
 USE customer_behavior;
+
+
 -- =====================================================
 -- 3️⃣ PROMOTION & DISCOUNT EFFECTIVENESS
 -- =====================================================
+
+
 -- High-Spending Customers Who Used Discounts
 -- Products with Highest Discount Dependency
 -- Customer Value vs Discount Usage
--- Discount Dependency by Customer Segment
+-- Discount Dependency by Customer Segment 
